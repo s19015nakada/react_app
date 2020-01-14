c1f8a76c35a8ec05e79f79800bcbbd9e59e2cc5a
ryusei <s19015@std.it-college.ac.jp>
2020-01-14 16:16:21 +0900

    value

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
-import React from './react'
+import React from 'react'
+
+const ItemValue = props => {
+  const item = 'SDカード'
+  const value = 1200
+  return (
+    <h1>
+      {item} - {value}
+    </h1>
+  )
+}
 
 class App extends React.Component {
     constructor(props) {
@@ -7,8 +17,9 @@ class App extends React.Component {
     }
     render () {
         return (
-            <div />
-
+            <div>
+                <ItemValue />
+            </div>
         )
     }
 }
