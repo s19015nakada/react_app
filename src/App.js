9ce8cac07520a8576867d233f959e635eee90b3b
ryusei <s19015@std.it-college.ac.jp>
2020-01-14 12:10:37 +0900

    syuusei

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from './react'
 
+const Greeting = props => {
+  const r = Math.floor(Math.random() * 3)
+  if (r == 0) return <p>今日も頑張りましょう</p>
+  if (r == 1) return <p>こんにちは。</p>
+  if (r == 2) return <p>朗らかな日ですね</p>
+}
+
 class App extends React.Component {
     constructor(props) {
         super(props)
@@ -7,8 +14,9 @@ class App extends React.Component {
     }
     render () {
         return (
-            <div />
-
+            <div>
+                <Greething />
+            </div>
         )
     }
 }
