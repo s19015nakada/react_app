79277521ab16d14c39e874ded29eb837409a35e5
ryusei <s19015@std.it-college.ac.jp>
2020-01-15 10:30:11 +0900

    syuusei

--- src/App.js
+++ src/App.js
@@ -1,4 +1,25 @@
-import React from './react'
+import React from 'react'
+
+const Maxim = props => {
+  return (
+    <div>
+      <p style={css1}>探すのに時があり</p>
+      <p style={css2}>捨てるのに時がある</p>
+    </div>
+  )
+}
+
+const css1 = {
+  color: 'red',
+  'background-color': '#f0f0ff',
+  'font-size': '2em'
+}
+
+const css2 = {
+  color: 'blue',
+  backgroundColor: '#fff0ff',
+  fontSize: '2em'
+}
 
 class App extends React.Component {
     constructor(props) {
@@ -6,10 +27,7 @@ class App extends React.Component {
         this.state = {}
     }
     render () {
-        return (
-            <div />
-
-        )
+        return <Maxim />
     }
 }
 
