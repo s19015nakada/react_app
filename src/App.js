a2c8295d64a0b1fbe58f1684719cd804fcfce6c1
ryusei <s19015@std.it-college.ac.jp>
2020-01-15 10:39:52 +0900

    syuusei

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React from './react'
+import React from 'react'
 
 class App extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        console.log('constructor')
     }
+
+    componentDidMount () {
+    console.log('componentDidMount')
+  }
+
+  shouldComponentUpdate (nextProps, nextState) {
+    console.log('shouldComponentUpdate')
+    return true
+  }
+
+  componentDidUpdate () {
+    console.log('componentDidUpdate')
+  }
+
+  componentWillUnmount () {
+    console.log('componentWillmount')
+  }
+
     render () {
+        console.log('render')
+        const setStateHandler = e => {
+          console.log('* call setState()')
+          this.setState({ r: Math.random() })
+    }
         return (
-            <div />
+            <div>
+              <button onClick={setStateHandler}>setState</button>
+            </div>
 
         )
     }
